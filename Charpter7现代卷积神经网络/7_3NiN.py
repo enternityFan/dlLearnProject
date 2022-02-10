52871445903593f94ddd377fcc16e886dc4504ee
enternityFan <51934710+enternityFan@users.noreply.github.com>
2022-02-10 18:27:48 +0800

    这是第七章的代码

---
+++ Charpter7现代卷积神经网络/7_3NiN.py
@@ -0,0 +1,42 @@
+# @Time: 10:55
+# @Author:Phalange
+# @File:7_3NiN.py
+# @Software:PyCharm
+# C’est la vie,enjoy it! :D
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+def nin_block(in_channels,out_channels,kernel_size,strides,padding):
+    return nn.Sequential(
+        nn.Conv2d(in_channels,out_channels,kernel_size,strides,padding),
+        nn.ReLU(),
+        nn.Conv2d(out_channels,out_channels,kernel_size=1),nn.ReLU(),
+        nn.Conv2d(out_channels,out_channels,kernel_size=1),nn.ReLU()
+    )
+
+
+
+if __name__ == "__main__":
+    net = nn.Sequential(
+        nin_block(1,96,kernel_size=11,strides=4,padding=0),
+        nn.MaxPool2d(3,stride=2),
+        nin_block(96,256,kernel_size=5,strides=1,padding=2),
+        nn.MaxPool2d(3,stride=2),
+        nin_block(256,384,kernel_size=3,strides=1,padding=1),
+        nn.MaxPool2d(3,stride=2),
+        nn.Dropout(0.5),
+        # 标签类别数是10
+        nin_block(384,10,kernel_size=3,strides=1,padding=1),
+        nn.AdaptiveAvgPool2d((1,1)),
+        # 将思维的输出转换为二维的输出，其性状为(批量大小，10)
+        nn.Flatten())
+
+    X = torch.rand(size=(1, 1, 224, 224))
+    for layer in net:
+        X = layer(X)
+        print(layer.__class__.__name__, 'output shape:\t', X.shape)
+
+    lr, num_epochs, batch_size = 0.1, 1, 32
+    train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size, resize=224)
+    d2l.train_ch6(net, train_iter, test_iter, num_epochs, lr, d2l.try_gpu())
